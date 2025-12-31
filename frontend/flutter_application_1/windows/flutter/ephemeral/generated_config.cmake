cc40bd8ae154d67924ea2e6fb2ef09820895c21f
raveesha gimhan <146311393+R3aveesha@users.noreply.github.com>
2025-12-31 09:02:20 +0530

    update

---
+++ frontend/flutter_application_1/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\USER\\OneDrive\\Desktop\\nextstop\\NextStop-Reaserach-frontend\\frontend\\flutter_application_1" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.1.0" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 0 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter"
+  "PROJECT_DIR=C:\\Users\\USER\\OneDrive\\Desktop\\nextstop\\NextStop-Reaserach-frontend\\frontend\\flutter_application_1"
+  "FLUTTER_ROOT=C:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\USER\\OneDrive\\Desktop\\nextstop\\NextStop-Reaserach-frontend\\frontend\\flutter_application_1\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\USER\\OneDrive\\Desktop\\nextstop\\NextStop-Reaserach-frontend\\frontend\\flutter_application_1"
+  "FLUTTER_TARGET=C:\\Users\\USER\\OneDrive\\Desktop\\nextstop\\NextStop-Reaserach-frontend\\frontend\\flutter_application_1\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzguNQ==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049ZjZmZjE1MjlmZA==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049MTUyN2FlMGVjNQ==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMC40"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\USER\\OneDrive\\Desktop\\nextstop\\NextStop-Reaserach-frontend\\frontend\\flutter_application_1\\.dart_tool\\package_config.json"
+)
